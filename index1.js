c2dac527626394f3c92b84d83f622e2f28ef24e4
Domala Sherpa <domalasherpa9813@gmail.com>
2024-03-10 19:37:31 +0545

    minor changes in error message

--- index1.js
+++ index1.js
@@ -3,75 +3,95 @@ const username = document.getElementById('username');
 const phonenumber = document.getElementById('phonenumber');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const password1 = document.getElementById('password1');
+const confirmPassword = document.getElementById('confirm-password');
+
 
 form.addEventListener('submit',e => {
     e.preventDefault();
 
     validateInputs();
 
-    const setError = (element, message) => {
-        const inputControl = element.parentElement;
-        const errorDisplay = inputControl.querySelector('.error');
-
-        errorDisplay.ennerText = message;
-        inputControl.classList.add('error');
-        inputControl.classList.remove('success')
-    }
-
-    const setSuccess = element => {
-        const inputControl = element.parentElement;
-        const errorDisplay = inputControl.querySelector('.error');
-    }
+    
 });
 
-const isValidEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-function phonenumber(inputtxt) {
-    var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    if(inputtxt.value.match(phoneno)) {
-	    return true;
-	    } else{
-	    alert("Not a valid Phone Number");
-	    return false;
+function validateInputs(){
+    const items = document.querySelectorAll(".item");
+    console.log(items.length);
+    for(const item of items){
+        if(item.value == ""){
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
         }
+
+        if(items[1].value != ""){
+            checkEmail(email);
+        }
+
+        if(items[3].value != ""){
+            checkPassword(password);
+        }
+
+        items[1].addEventListener("keyup", ()=>{
+            checkEmail(email);
+        })
+
+        items[3].addEventListener("keyup", ()=>{
+            checkPassword(password);
+        })
+
+        if(items[4].value != ""){
+            passwordMatch(password, confirmPassword);
+        }
+
+        items[4].addEventListener("keyup", ()=>{
+            checkPassword(password);
+        })
+
+        item.addEventListener("keyup", ()=>{
+            if(item.value != ""){
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else{
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        })
+    }
 }
 
-
-const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password1Value = password1.value.trim();
-
-    if(usernameValue === '') {
-        setError(username, 'Username is required');
-    } else {
-        setSuccess(username);
+function checkEmail(email){
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+    if(!email.value.match(emailRegex)){
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
     }
-
-    if(emailValue === '') {
-        setError(email, 'Email is required');
-    } else if(!isValidEmail(emailValue)) {
-        setError(email, 'Provide a valid email address');
+    else{
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
     }
+}
 
-    if(passwordValue === '') {
-        setError(password, 'Password is required');
-    } else if(passwordValue.length<8){
-        setError(password, 'Password must be at least 8 character.');
-    } else {
-        setSuccess(password);
+function checkPassword(password){
+    const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/;
+
+    if(!password.value.match(passwordRegex)){
+        password.classList.add("error");
+        password.parentElement.classList.add("error");
     }
+    else{
+        password.classList.remove("error");
+        password.parentElement.classList.remove("error");
+    }
+}
 
-    if(password1Value === '') {
-        setError(password1, 'Please confirm your password');
-    } else if (password1Value !== passwordValue) {
-        setError(password1, "Password doesn't match");
-    } else {
-        setSuccess(password1);
+function passwordMatch(password1, password2){
+    if(password1.value !== password2.value){
+        password2.classList.add("error");
+        password2.parentElement.classList.add("error");
+    }
+    else{
+        password2.classList.remove("error");
+        password2.parentElement.classList.remove("error");
     }
 }
