cea3ac2486502ba78ad7903da85ef805c00f2d87
domalasherpa <domalasherpa9813@gmail.com>
2024-03-29 19:04:16 +0545

    Update elections.js

--- backend/routes/elections.js
+++ backend/routes/elections.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const electionController = require('../controllers/electionController');
 
 // Get all elections
-router.get('/getallcategories', electionController.getAllElections);
+router.get('/getallelections', electionController.getAllElections);
 //get election by id
 router.get('/getelection/:id', electionController.getCustomerById);
 // Create a new election
 router.post('/create-election', electionController.createElection);
 // update election by id
 router.put('/update-election/:id', electionController.updateElection);
-// delete category by id
+// delete election by id
 router.delete('/delete-election/:id', electionController.deleteElection);
 
 module.exports = router;
