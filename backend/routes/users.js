e940aa8116b328005174ffb7308aa56748a42eb3
domalasherpa <domalasherpa9813@gmail.com>
2024-03-29 19:04:58 +0545

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 const userController = require('../controllers/userController');
 
-// Get all elections
+// Get all users
 router.get('/getallusers', userController.getAllUsers);
-//get election by id
+//get user by id
 router.get('/getuser/:id', userController.getUserById);
-// Create a new election
-router.post('/create-user', candidateController.createUser);
-// update election by id
-router.put('/update-user/:id', electionController.updateUser);
-// delete category by id
-router.delete('/delete-user/:id', electionController.deleteUser);
+// Create a new user
+router.post('/create-user', userController.createUser);
+// update user by id
+router.put('/update-user/:id', userController.updateUser);
+// delete user by id
+router.delete('/delete-user/:id', userController.deleteUser);
 
 module.exports = router;
