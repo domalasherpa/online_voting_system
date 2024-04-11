2ab107a676423fb481b8c3e367c41783bf1c974f
CCR <83866850+SishirDangi@users.noreply.github.com>
2024-04-11 08:07:41 +0545

    67 integration of sprint 5 (#72)
    * completed (#70)
    
    singlecandidate
    
    Co-authored-by: amit8710 <83849830+amit8710@users.noreply.github.com>
    
    * 58 get all elections (#69)
    
    * Get all user details
    
    Get all the users from database and show their basic details in frontend
    
    * get all candidates
    
    done
    
    * get all elections
    
    ---------
    
    Co-authored-by: SishirDangi <shishirdangi541@gmail.com>
    
    * 64 create routes for election candidates and users (#71)
    
    * basic user route
    
    * basic election route
    
    * basic candidates  route
    
    * Rename user.js to auth.js
    
    * name change to auth.js
    
    * user.js to auth.js
    
    * Update candidates.js
    
    * Update candidates.js
    
    * Update elections.js
    
    * Update users.js
    
    * Get all user details (#74)
    
    Get all the users from database and show their basic details in frontend
    
    Co-authored-by: SishirDangi <shishirdangi541@gmail.com>
    
    * get all candidates (#73)
    
    done
    
    Co-authored-by: SishirDangi <shishirdangi541@gmail.com>
    
    ---------
    
    Co-authored-by: domalasherpa <domalasherpa9813@gmail.com>
    Co-authored-by: amit8710 <83849830+amit8710@users.noreply.github.com>

---
+++ backend/routes/candidates.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const candidateController = require('../controllers/candidateController');
+
+// Get all candidates
+router.get('/getallcandidates', candidateController.getAllCandidates);
+//get candidate by id
+router.get('/getcandidate/:id', candidateController.getCandidateById);
+// Create a new candidate
+router.post('/create-candidate', candidateController.createCandidate);
+// update candidate by id
+router.put('/update-candidate/:id', candidateController.updateCandidate);
+// delete candidate by id
+router.delete('/delete-candidate/:id', candidateController.deleteCandidate);
+
+module.exports = router;
