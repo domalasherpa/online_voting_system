20d0ab9abd542cf86e65372af36c0b045d1075ba
Domala Sherpa <domalasherpa9813@gmail.com>
2024-04-18 12:55:09 +0545

    reset password script done

--- backend/public/js/resetPassword.js
+++ backend/public/js/resetPassword.js
@@ -1,49 +1,78 @@
-import {passwordMatch, checkPassword} from '../../../views/scripts/validations.js';
+function passwordMatch(password1, password2) {
+	if (password1.value !== password2.value) {
+		password2.classList.add("error");
+		password2.parentElement.parentElement.classList.add("error");
+	} else {
+		password2.classList.remove("error");
+		password2.parentElement.parentElement.classList.remove("error");
+	}
+}
 
-export default function resetPassword(){
-    document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
+function checkPassword(password) {
+	const passwordRegex =
+		/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/;
+	if (!password.value.match(passwordRegex)) {
+		password.classList.add("error");
+		password.parentElement.parentElement.classList.add("error");
+	} else {
+		password.classList.remove("error");
+		password.parentElement.parentElement.classList.remove("error");
+	}
+}
 
-    form.addEventListener("submit", async function (event) {
-      event.preventDefault(); // Prevent the default form submission
+function checkChange(item){
+	item.addEventListener("keyup", ()=>{
+		if(item.value != ""){
+			item.classList.remove("error");
+			item.parentElement.parentElement.classList.remove("error");
+		}
+		else{
+			item.classList.add("error");
+			item.parentElement.parentElement.classList.add("error");
+		}
+	})
+}
 
-      // Retrieve password and confirm password values from the form
-      const password = passwordField.value;
-      const confirmPassword = confirmpassword.value;
+export default function resetPassword(password, confirmPassword, id, token) {
+	document.addEventListener("DOMContentLoaded", function () {
+		const form = document.querySelector("form");
 
-      // Validate password and confirm password fields
-      if (password !== confirmPassword) {
-        alert("Passwords do not match");
-        return;
-      }
-
-    //   // Construct the data object to send in the request
-    //   const data = {
-    //     password: password,
-    //     password2: confirmPassword,
-    //   };
-
-      try {
-        // Make a POST request to your backend endpoint to reset the password
-        const response = await fetch(
-          "/api/v1/reset-password/" + "<%-id%>" + "/" + "<%- token %>",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-          }
-        );
-        // Check if the request was successful
-        const result = response.json();
-        alert(result.message);
-        if (response.success) {
-          window.location = '/views/markup/login.html";';
-        }
-      } catch (error) {
-        console.error("Error:", error);
-        alert("An error occurred while resetting the password"); // Show generic error message
-      }
-    });
-  })};
+		form.addEventListener("submit", async function (event) {
+			event.preventDefault(); // Prevent the default form submission
+			if (password.value != "") {
+				checkPassword(password);
+			}
+			if(confirmPassword.value != ""){
+				passwordMatch(password, confirmPassword);
+			}
+			checkChange(password);
+			checkChange(confirmPassword);
+			const data = {
+				password: password.value,
+				password2: confirmPassword.value
+			};
+			
+			if(!password.classList.contains('error') && !confirmPassword.classList.contains('error')){
+				try {
+					// Make a POST request to your backend endpoint to reset the password
+					const response = await fetch(`/api/v1/reset-password/${id}/${token}`, {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify(data),
+					});
+					// Check if the request was successful
+					const result = await response.json();
+					alert(result.message);
+					if (result.success) {
+						window.location = "/frontend/markup/login.html";
+					}
+				} catch (error) {
+					alert("An error occurred while resetting the password"); // Show generic error message
+				}
+			}
+			
+		});
+	});
+}
