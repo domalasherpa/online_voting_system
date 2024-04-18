ecdbf43f9e83efbe048d1bd3b78f8d53953cc069
Domala Sherpa <domalasherpa9813@gmail.com>
2024-04-18 13:49:17 +0545

    minor changes in the directory of the static files

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,8 @@ const cookieParser = require('cookie-parser');
 const multer = require('multer');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));
-app.use(express.static(path.join(__dirname, '../views')));
+app.set('views', path.join(__dirname, './views'));
+app.use(express.static(path.join(__dirname, './public')));
 
 require('dotenv').config()
 const PORT = process.env.PORT || 4000
