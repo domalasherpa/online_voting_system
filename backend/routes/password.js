156d2dd4cf1882191d56c2db4c241164d969c81c
Domala Sherpa <domalasherpa9813@gmail.com>
2024-04-18 13:41:09 +0545

    basic routes for password related tasks

--- backend/routes/password.js
+++ backend/routes/password.js
@@ -1,31 +1,18 @@
-
 const express = require('express');
 const router = express.Router();
 
 const {sendResetLink, verifyPasswordToken, sendResetPasswordPage} = require('../controllers/resetpassword.js');
 
-router.get("/forget-password", (req , res, next)=>{
-    // res.end("this is forgot pass");
-    res.render("forget-password");
-})
-
+router.get("/forget-password", (req , res, next)=>{res.render("pages/forget-password");});
 router.post("/send-password-reset", sendResetLink); //send links from the backend
 router.get("/reset-password/:id/:token", sendResetPasswordPage);  //request for the reset password page./ rendering
 router.post("/reset-password/:id/:token", verifyPasswordToken, (req, res, next)=>{
-    if(res.locals.status == 200){
-        //give password reset sucessfully message
-        //redirect to login
+    if(res.locals.status == 200){ 
         return res.status(200).json({'success': true, 'message': 'Password reset sucessfully.'})
     }
     else{
-        //throw error and its handler in fs
         return res.status(500).json({'success': false, 'message': 'Password reset failed.'})
-
     }
 });  
-
-router.all('/', (req, res, next)=>{
-    console.log("Internal server error");
-})
 
 module.exports = router;
