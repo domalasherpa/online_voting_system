dadc04c343aace239d6bad644a29391ebda02ac0
Domala Sherpa <domalasherpa9813@gmail.com>
2024-04-19 20:33:54 +0545

    minor changes in the election file

--- backend/routes/elections.js
+++ backend/routes/elections.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const electionController = require('../controllers/electionController');
 
 // Get all elections
-router.get('/getallelections', electionController.getAllElections);
-//get election by id
-router.get('/getelection/:id', electionController.getCustomerById);
+router.get('/elections', electionController.getAllElections);
+// Get election by id
+router.get('/elections/:id', electionController.getElectionById);
 // Create a new election
-router.post('/create-election', electionController.createElection);
-// update election by id
-router.put('/update-election/:id', electionController.updateElection);
-// delete election by id
-router.delete('/delete-election/:id', electionController.deleteElection);
+router.post('/elections', electionController.createElection);
+// Update election by id
+router.put('/elections/:id', electionController.updateElection);
+// Delete election by id
+router.delete('/elections/:id', electionController.deleteElection);
 
 module.exports = router;
