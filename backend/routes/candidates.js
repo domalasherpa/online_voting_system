863b7384a35f015ba84d78fb423db1bf22d606c4
Domala Sherpa <domalasherpa9813@gmail.com>
2024-04-25 10:18:15 +0545

    routes url change in candidate

--- backend/routes/candidates.js
+++ backend/routes/candidates.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const candidateController = require('../controllers/candidateController');
 
 // Get all candidates
-router.get('/getallcandidates', candidateController.getAllCandidates);
-//get candidate by id
-router.get('/getcandidate/:id', candidateController.getCandidateById);
+router.get('/candidates', candidateController.getAllCandidates);
+// Get candidate by id
+router.get('/candidates/:id', candidateController.getCandidateById);
 // Create a new candidate
-router.post('/create-candidate', candidateController.createCandidate);
-// update candidate by id
-router.put('/update-candidate/:id', candidateController.updateCandidate);
-// delete candidate by id
-router.delete('/delete-candidate/:id', candidateController.deleteCandidate);
+router.post('/candidates', candidateController.createCandidate);
+// Update candidate by id
+router.put('/candidates/:id', candidateController.updateCandidate);
+// Delete candidate by id
+router.delete('/candidates/:id', candidateController.deleteCandidate);
 
 module.exports = router;
