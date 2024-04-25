392088b77e38c2eb83f2dc5372bccc693a580de7
Domala Sherpa <domalasherpa9813@gmail.com>
2024-04-25 10:33:51 +0545

    routes url change in user route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 // Get all users
-router.get('/getallusers', userController.getAllUsers);
-//get user by id
-router.get('/getuser/:id', userController.getUserById);
+router.get('/users', userController.getAllUsers);
+// Get user by id
+router.get('/users/:id', userController.getUserById);
 // Create a new user
-router.post('/create-user', userController.createUser);
-// update user by id
-router.put('/update-user/:id', userController.updateUser);
-// delete user by id
-router.delete('/delete-user/:id', userController.deleteUser);
+router.post('/users', userController.createUser);
+// Update user by id
+router.put('/users/:id', userController.updateUser);
+// Delete user by id
+router.delete('/users/:id', userController.deleteUser);
 
 module.exports = router;
